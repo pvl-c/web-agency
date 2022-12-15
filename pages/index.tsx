8f0da06363fdcf7621a15d03f562b823c7a2134e
pavelchachotkin <pvl.c.bln@gmail.com>
2022-12-15 12:21:59 +0100

    minor changes. fix title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
+import Head from "next/head";
 import { Sidenav } from "../components/shared/sidenav";
 import { StartView } from "../components/start-view";
 
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>YWA</title>
+    </Head>
     <div className='flex w-full h-full flex-grow'>
-    <Sidenav />
-    <div className='w-full'>
-    <StartView />
-    </div>
+      <Sidenav />
+      <div className='w-full'>
+      <StartView />
+      </div>
     </div>
     </>
   )
